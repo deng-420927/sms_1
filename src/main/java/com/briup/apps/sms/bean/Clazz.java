8f1462bfc9065f49355815a40d1ccf2c56f7a003
胡颖蝶 <1281857171@qq.com>
2019-07-11 00:03:45 +0800

    [胡颖蝶]提交代码

---
+++ src/main/java/com/briup/apps/sms/bean/Clazz.java
@@ -0,0 +1,26 @@
+package com.briup.apps.sms.bean;
+
+public class Clazz {
+	private Long id;
+	private String name;
+	private String begin_time;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBegin_time() {
+		return begin_time;
+	}
+	public void setBegin_time(String begin_time) {
+		this.begin_time = begin_time;
+	}
+	
+}
