6b9c9ed98e4d88d5ad0be97f6fdc714605c2897f
黄佳军 <1604799954qq.com>
2019-07-11 13:56:03 +0800

    【黄佳军】更新，删除，查询代码的编写

--- src/main/java/com/briup/apps/sms/web/controller/CourseController.java
+++ src/main/java/com/briup/apps/sms/web/controller/CourseController.java
@@ -4,18 +4,33 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.briup.apps.sms.bean.Course;
+
 import com.briup.apps.sms.service.CourseService;
 
 @RestController
 @RequestMapping("/course")
 public class CourseController {
-
+	
 	@Autowired
 	private CourseService courseService;
+	@PostMapping("saveOrUpdate")
+	public String saveOrUpdate(Course course)
+	{
+		
+		try {
+			courseService.saveOrUpdate(course);
+			return "更新成功";
+	}catch(Exception e)
+	{	
+		e.printStackTrace();
+		return "失败";
+	}
+	}
 	@GetMapping("selectAll")
 	public List<Course> selectAll()
 	{
